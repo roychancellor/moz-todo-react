8d7581d1994921aeba58c546670658e0aa0083ec
rchancellor <roychance600@gmail.com>
2023-09-04 15:16:38 -0700

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,119 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef, useEffect, useState } from "react";
+import Form from "./components/Form"
+import FilterButton from "./components/FilterButton"
+import Todo from "./components/Todo";
+import { nanoid } from "nanoid";
 
-function App() {
+function usePrevious(value) {
+  const ref = useRef();
+  useEffect(() => {
+    ref.current = value;
+  });
+  return ref.current;
+}
+
+// These functions will serve as predicates to the filter function to show only certain tasks
+// They are global to the app because they will not change, so we don't want them rendered
+// every time the app changes state.
+const FILTER_MAP = {
+  All: () => true,
+  Active: (task) => !task.completed,
+  Completed: (task) => task.completed,
+};
+const FILTER_NAMES = Object.keys(FILTER_MAP);
+
+function App(props) {
+  // These are hooks that allow the App component to have state
+  const [tasks, setTasks] = useState(props.tasks);
+  const [filter, setFilter] = useState("All");
+  const listHeadingRef = useRef(null);
+  
+  // This callback function gets passed into Form (below) as part of props
+  // The Form component then has access to the function via its props and can
+  // call the function as any other function. When Form calls addTask,
+  // whatever code the function defined here will run.
+  function addTask(name) {
+      const newTask = { id: `todo-${nanoid()}`, name, completed: false };
+      setTasks([...tasks, newTask]);
+  }
+  function deleteTask(id) {
+    const remainingTasks = tasks.filter((task) => id !== task.id);
+    setTasks(remainingTasks);
+  }  
+  function toggleTaskCompleted(id) {
+    const updatedTasks = tasks.map((task) => {
+      if (id === task.id){
+        return {...task, completed: !task.completed };
+      }
+      return task;
+    });
+    setTasks(updatedTasks);
+  }
+  function editTask(id, newName) {
+    const editedTaskList = tasks.map((task) => {
+      // if this task has the same ID as the edited task
+      if (id === task.id) {
+        //
+        return { ...task, name: newName };
+      }
+      return task;
+    });
+    setTasks(editedTaskList);
+  }
+  
+  // The props comes from index.js where it renders App and passes tasks={DATA}
+  // The function iterates through DATA and maps its contents to the props for
+  // the Todo component. Note the filter function is applied first to invoke
+  // the lambda function in FILTER_MAP with the key of the filter const ("All", "Active", or "Completed").
+  // The lambda provides the filter predicate.
+  const taskList = tasks
+    .filter(FILTER_MAP[filter])
+    .map((task) => (
+      <Todo
+        id={task.id}
+        name={task.name}
+        completed={task.completed}
+        key={task.id}
+        toggleTaskCompleted={toggleTaskCompleted}
+        deleteTask={deleteTask}
+        editTask={editTask}
+      />
+  ));
+
+  const tasksNoun = taskList.length !== 1 ? "tasks" : "task";
+  const listHeadingText = `${taskList.length} ${tasksNoun} remaining`;
+  const filterList = FILTER_NAMES.map((name) => (
+    <FilterButton
+      key={name}
+      name={name}
+      isPressed={name === filter}
+      setFilter={setFilter}
+    />
+  ));  
+  
+  const prevTaskLength = usePrevious(tasks.length);
+  useEffect(() => {
+    if (tasks.length - prevTaskLength === -1) {
+      listHeadingRef.current.focus();
+    }
+  }, [tasks.length, prevTaskLength]);  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="todoapp stack-large">
+      <h1>TodoMatic</h1>
+      <Form addTask={addTask}/>
+      <div className="filters btn-group stack-exception">
+        {filterList}
+      </div>
+      <h2 id="list-heading" tabIndex="-1" ref={listHeadingRef}>
+        {listHeadingText}
+      </h2>
+      <ul
+        role="list"
+        className="todo-list stack-large stack-exception"
+        aria-labelledby="list-heading">
+        {taskList}
+      </ul>
     </div>
   );
 }
